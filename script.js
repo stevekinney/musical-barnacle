2763de6cccbb28163472dd415374bf532a339027
Steve Kinney <hello@stevekinney.net>
2016-07-11 10:04:36 -0600

    Add very important math functions

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+  returns a - b;
+}
+
+function subtract(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return Math.pow(a, 2);
+}
+
+function divide(a, b) {
+  a / b;
+}
